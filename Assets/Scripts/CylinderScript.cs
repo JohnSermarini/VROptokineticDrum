b2e54d9b211f7ddf87cc6258b26a22c5f35ac9ad
John Sermarini <JohnSermarini@knights.ucf.edu>
2017-11-21 15:41:53 -0500

    Cleaned code/scene. Added executable file.

---
+++ Assets/Scripts/CylinderScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CylinderScript : MonoBehaviour
+{
+    private GameObject infoObject;
+	private bool moving = false;
+	private float turnSpeed = 30f;
+
+    private void Start()
+    {
+        infoObject = GameObject.Find("Info");
+    }
+
+    private void Update () 
+	{
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if(moving == false && Input.GetKey(KeyCode.Space))
+        {
+            infoObject.SetActive(false);
+            moving = true;
+        }
+        else if(moving == true)
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+	}
+}
